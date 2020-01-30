a77d14e0a40e7a36106c0e34b5306677ff340b5e
haloreach252 <haloreach252@gmail.com>
2020-01-30 10:02:02 -0700

    Initial commit

---
+++ NumberGuesser/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace NumberGuesser {
+	class Program {
+		static void Main(string[] args) {
+			int min = 0, max = 0;
+			if (args.Length == 2) {
+				int.TryParse(args[0], out min);
+				int.TryParse(args[1], out max);
+			}
+
+			if (min == 0) min = 1;  if (max == 0) max = 100;
+			if (min >= max) min = max - 1;
+			new Program().Game(min, max);
+		}
+
+		private void Game(int min, int max) {
+			Console.WriteLine("Guess a number");
+			int ran = new Random().Next(min, max + 1), attempts = 1, ans;
+			int.TryParse(Console.ReadLine(), out ans);
+			while (ans != ran) {
+				attempts++;
+				Console.WriteLine(ans > ran ? $"You guessed {ans}. Thats too big!" : $"You guessed {ans}. Thats too small");
+				int.TryParse(Console.ReadLine(), out ans);
+			}
+			Console.WriteLine($"You guessed {ans}. You won! It took {attempts} attempts.\nPlay again (yes/no)?");
+			if(Console.ReadLine().ToLower() == "yes") Game(min, max);
+			else return;
+		}
+
+	}
+}
---
+++ PigLatin/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PigLatin {
+	class Program {
+		static void Main(string[] args) {
+			string str1, str2;
+			Console.WriteLine("Enter a word to pig latinify: ");
+			str1 = Console.ReadLine();
+			str2 = str1.Substring(0, 1);
+			Console.WriteLine($"Your new word is: {str1.Substring(1)} {str2}ay");
+			Console.ReadKey();
+		}
+	}
+}
